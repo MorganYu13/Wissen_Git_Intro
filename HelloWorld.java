6e2a7cc51dbba61d443b137d83326fb75a7718ac
Angelin Ann <angelinann.jacob@gmail.com>
2023-02-13 22:45:18 +0530

    Added a new println from the new_branch branch

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,6 +1,7 @@
 class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello, World!"); 
-        System.out.println("New Change made here!"); 
+        System.out.println("Hello, World!");
+        System.out.println("New Change made here!");
+        System.out.println("Added this line from new_branch (NOT master)");
     }
 }
